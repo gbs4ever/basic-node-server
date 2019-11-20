9c66e01db9f230f19faa45c340ef108caf6e2691
gbs4ever <gbs4ever2@gmail.com>
2019-11-20 01:21:14 -0500

    server css styles

--- node-server/app.js
+++ node-server/app.js
@@ -3,15 +3,16 @@ const app = express()
 const parser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
+const path = require('path')
 
-
+const rootDir = require('./util/path')
 
 app.use(parser.urlencoded({extended: false}))
 app.use(adminRoutes);
 app.use(shopRoutes);
-
+ app.use( express.static(path.join(rootDir,'public')));
 app.use((req,res,next)=>{
-  res.status(404).send("<h1>Page not found 404</h1>")
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 app.listen(3000)
 console.log('server has loaded on port 3000');
