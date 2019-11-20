9dabb139be5901c1a0ba60ed288e76a8f59b4684
gbs4ever <gbs4ever2@gmail.com>
2019-11-20 01:23:22 -0500

    add prodcuts route

--- node-server/routes/shop.js
+++ node-server/routes/shop.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const path = require('path')
 const router = express.Router()
 
-router.use('/', express.static('./public'));
 
+
+router.get('/',(req,res,next)=>{
+  res.sendFile(path.join(__dirname,'../','views','index.html'))
+
+})
+router.get('/product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'product.html'))
+
+})
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports= router
