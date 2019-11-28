52febb46f823aeb32f8136eb1edd55d51088131d
gbs4ever <gbs4ever2@gmail.com>
2019-11-28 14:15:36 -0500

    changed routes for root url

--- node-server/routes/shop.js
+++ node-server/routes/shop.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const path = require('path')
-const router = express.Router()
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const data = require("./admin");
 
-
-
-router.get('/',(req,res,next)=>{
-  res.sendFile(path.join(__dirname,'../','views','index.html'))
-
-})
-router.get('/product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'product.html'))
-
-})
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+router.get("/", (req, res, next) => {
+  const products = data.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
-module.exports= router
+module.exports = router;
+
+console.log(data.products);
