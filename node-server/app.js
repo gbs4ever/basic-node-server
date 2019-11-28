7644d37dfc3fb7943974f609aea36713cf4b289f
gbs4ever <gbs4ever2@gmail.com>
2019-11-28 14:13:15 -0500

    fixed missing root

--- node-server/app.js
+++ node-server/app.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const parser = require('body-parser')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop');
-const path = require('path')
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+const parser = require("body-parser");
+const adminData = require("./routes/admin");
+const shopData = require("./routes/shop");
+const path = require("path");
 
-const rootDir = require('./util/path')
+const rootDir = require("./util/path");
 
-app.use(parser.urlencoded({extended: false}))
-app.use(adminRoutes);
-app.use(shopRoutes);
- app.use( express.static(path.join(rootDir,'public')));
-app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
-})
-app.listen(3000)
-console.log('server has loaded on port 3000');
+app.use(parser.urlencoded({ extended: true }));
+app.use(adminData.routes);
+app.use(shopData);
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+app.listen(3000);
+console.log("server has loaded on port 3000");
